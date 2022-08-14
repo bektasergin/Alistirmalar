1ab998c6b6bc50d664c3aa3d0402d9994ca3ac1b
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:04:11 +0300

    Add files via upload

---
+++ Alan ve cevre hesaplama/Alan ve cevre hesaplama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Alan_ve_cevre_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dikdörtgenin kısa kenarını giriniz: ");
+            int kısa = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Dikdörtgenin uzun kenarını giriniz: ");
+            int uzun = Convert.ToInt32(Console.ReadLine());
+
+            int alan = (kısa * uzun);
+            int cevre = ((kısa + uzun) * 2);
+
+            Console.WriteLine("Dikdörtgenin Alanı: ");
+            Console.WriteLine(alan);
+
+            Console.WriteLine("Dikdörtgenin Çevresi: ");
+            Console.WriteLine(cevre);
+
+            Console.ReadKey();
+        }
+    }
+}
