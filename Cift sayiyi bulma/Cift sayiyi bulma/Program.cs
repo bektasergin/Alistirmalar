5ef3a40b70d4bf0086cbf5e9311504302fc5dafa
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:06:20 +0300

    Add files via upload

---
+++ Cift sayiyi bulma/Cift sayiyi bulma/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Cift_sayiyi_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi%2==0)
+            {
+                Console.WriteLine("{0} bir çift sayıdır. ",sayi);
+            }
+            else
+            {
+                Console.WriteLine("{0} bir çift sayı değildir. ", sayi);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
