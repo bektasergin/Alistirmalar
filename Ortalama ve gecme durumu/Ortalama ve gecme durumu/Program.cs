19f20a2d1c334be79d406cbce753a65812f7ff28
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:07:21 +0300

    Add files via upload

---
+++ Ortalama ve gecme durumu/Ortalama ve gecme durumu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ortalama_ve_gecme_durumu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen birinci yazılıyı giriniz: ");
+            int yazili = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Lütfen ikinci yazılıyı giriniz: ");
+            int yazili1 = Convert.ToInt32(Console.ReadLine());
+
+            int ortalama = (yazili + yazili1) / 2;
+
+            Console.Write("Öğrencinin ortalaması: ");
+            Console.WriteLine(ortalama);
+
+            if (ortalama>=50)
+            {
+                Console.Write("Öğrenci geçmiştir. ");
+            }
+            else
+            {
+                Console.Write("Öğrenci geçememiştir. ");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
