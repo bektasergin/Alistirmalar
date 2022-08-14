a0e1a4b9b83e94d95681dfe95c6f10c9185e6a75
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:05:13 +0300

    Add files via upload

---
+++ Araligini bulma/Araligini bulma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Araligini_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi>=0 && sayi<=100)
+            {
+                Console.WriteLine("{0} 0 ve 100 arasındadır. ",sayi);
+            }
+            else
+            {
+                Console.WriteLine("{0} 0 ve 100 arasında değildir. ",sayi);
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
