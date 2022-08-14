32212f8ed471874f9083276cfc30293349efc9ac
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:03:31 +0300

    Add files via upload

---
+++ 50 den kücük olanlarin adetini bulma/50 den kücük olanlarin adetini bulma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _50_den_kücük_olanlarin_adetini_bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, adet = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.Write(i+".sayıyı giriniz: ");
+                sayi = Int32.Parse(Console.ReadLine());
+                if (sayi<50)
+                {
+                    adet++;
+                }
+            }
+
+            Console.WriteLine("50 den küçük girilen sayı adeti: "+adet);
+            Console.ReadKey();
+
+        }
+
+    }
+}
