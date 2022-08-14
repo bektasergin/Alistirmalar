52116af9cf9d48d488bd684100152658aab79678
bektasergin <111241864+bektasergin@users.noreply.github.com>
2022-08-14 20:08:23 +0300

    Add files via upload

---
+++ Yuzde degerini hesaplama/Yuzde degerini hesaplama/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Yuzde_degerini_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Lütfen bir yüzdelik giriniz: ");
+            int yüzdelik = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("{0}'ın %{1}'i: ",sayi,yüzdelik);
+            Console.WriteLine(sayi*yüzdelik/100);
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
